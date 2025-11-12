a7fbb64151ff84f05b7ad3a182bb4f268f4eeb21
Andy <andyduong03@gmail.com>
2025-11-11 17:16:49 -0800

    Fix WAVE errors

--- hw3/src/pages/List.js
+++ hw3/src/pages/List.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import getCountries from '../Api.js';
+import React, { useEffect, useState } from "react";
+import getCountries from "../Api.js";
 
 export default function List() {
   const [countries, setCountries] = useState([]);
@@ -14,22 +14,25 @@ export default function List() {
 
   return (
     <div className="country-list">
-      {countries.map((c) => (
-        <div key={c.cca3} className="country-card">
-          <img src={c.flags?.svg || ''} />
-          <h3>{c.name.common}</h3>
-          <p>
-            <strong>Capital:</strong> {c.capital?.[0] || 'N/A'}
-          </p>
-          <p>
-            <strong>Population:</strong>{' '}
-            {c.population?.toLocaleString() || 'N/A'}
-          </p>
-          <p>
-            <strong>Timezones:</strong> {c.timezones?.join(', ') || 'N/A'}
-          </p>
-        </div>
-      ))}
+      <h1>List of South America countries</h1>
+      <div className="country-list">
+        {countries.map((c) => (
+          <div key={c.cca3} className="country-card">
+            <img src={c.flags?.svg || ""} alt={`${c.name.common} flag`} />
+            <h2>{c.name.common}</h2>
+            <p>
+              <strong>Capital:</strong> {c.capital?.[0] || "N/A"}
+            </p>
+            <p>
+              <strong>Population:</strong>{" "}
+              {c.population?.toLocaleString() || "N/A"}
+            </p>
+            <p>
+              <strong>Timezones:</strong> {c.timezones?.join(", ") || "N/A"}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
